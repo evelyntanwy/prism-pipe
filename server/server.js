c1b75a50b8ae4574cf97d4ccfb0b9c75932b1b3b
Evelyn Tan <evetan.wy@gmail.com>
2022-03-04 21:08:03 +1300

    setting up server side

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const server = express();
+
+const goalsRoutes = require("./routes/goals");
+
+server.use(express.json());
+server.use(express.static(path.join(__dirname, "public")));
+server.use(express.urlencoded({ extended: true }));
+
+server.use("/api/v1/goals", goalsRoutes);
+
+server.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+module.exports = server;
